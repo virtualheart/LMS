ae239d4afb1a198cdf2fd038e923a0ff2e0d2151
Virtualheart <dhanajayan99@gmail.com>
2022-02-28 12:21:40 +0530

    Small changes

--- admin/delete_books.php
+++ admin/delete_books.php
@@ -1,13 +1,25 @@
 <?php
+
 	include"../include/config.php";
+
 	
+
 	$qry="delete from books where bid='{$_GET["id"]}'";
+
+
 
 	if ($_SESSION["role"]='admin') {
 
-		if($con->query($qry) && @$_SESSION["role"]=='admin')
+
+
+		if($con->query($qry))
+
 		{
+
 			header("location:view_books.php?msg=Book Deleted Successfully...!!!");
+
 		}
+
 	}
+
 ?>
--- staff/staff_delete.php
+++ staff/staff_delete.php
@@ -1,11 +1,21 @@
 <?php
+
 	include"../include/config.php";
+
 	session_start();
-	include"./staff_security.php"
+
+if ($_SESSION["role"]='admin') {
 
 	echo $qry="delete from staff where sid={$_GET['id']}";
+
 	if($con->query($qry))
+
 	{
+
 		header("location:view_staff.php?msg=Delete Successfully");		
+
 	}
+	}
+	
+
 ?>
