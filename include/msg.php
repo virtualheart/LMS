b928a4a0883919a4f7de05d3bb6aa50a2094a0f1
virtualheart <dhanajayan99@gmai.com>
2022-03-01 16:19:17 +0530

    mgs file added

---
+++ include/msg.php
@@ -0,0 +1,68 @@
+<?php
+
+$mgsarr = array();
+
+$mgsarr[0]  = "Return Date Update Successfully";
+$mgsarr[1]  = "Book Deleted Successfully...!!!";
+$mgsarr[2]  = "Deleted Successfully...!!!";
+$mgsarr[3]  = "Return Details Updated Successfuly...!!!";
+$mgsarr[4]  = "Book Updated Successfully...!!!";
+$mgsarr[5]  = "Update Successfully...!!!";
+$mgsarr[6]  = "Update Falied...!!!";
+$mgsarr[7]  = "Request Send Successfully..!!!";
+$mgsarr[8]  = "Request Cancelled...!!!";
+$mgsarr[9]  = "Request Send Successfully..!!!";
+$mgsarr[10] = "";
+$mgsarr[11] = "";
+$mgsarr[12] = "";
+$mgsarr[13] = "";
+$mgsarr[14] = "";
+$mgsarr[15] = "";
+$mgsarr[16] = "";
+$mgsarr[17] = "";
+$mgsarr[18] = "";
+$mgsarr[19] = "";
+$mgsarr[10] = "";
+$mgsarr[21] = "";
+$mgsarr[21] = "";
+$mgsarr[23] = "";
+$mgsarr[24] = "";
+$mgsarr[25] = "";
+$mgsarr[26] = "";
+$mgsarr[27] = "";
+$mgsarr[28] = "";
+$mgsarr[29] = "";
+$mgsarr[20] = "";
+$mgsarr[30] = "";
+$mgsarr[31] = "";
+$mgsarr[32] = "";
+$mgsarr[33] = "";
+$mgsarr[34] = "";
+$mgsarr[35] = "";
+$mgsarr[36] = "";
+$mgsarr[37] = "";
+$mgsarr[38] = "";
+$mgsarr[39] = "";
+$mgsarr[40] = "";
+$mgsarr[41] = "";
+$mgsarr[42] = "";
+$mgsarr[43] = "";
+$mgsarr[44] = "";
+$mgsarr[45] = "";
+
+//echo $mgsarr[$_GET['mgs']];
+
+//example use the code
+/*
+* filename.php?msg=1
+* 
+* include ./include/msg.php;
+* 
+* if (isset($_GET['mgs'])) {
+*	//	echo $mgsarr[$_GET['mgs']];
+* }
+*
+*/
+
+
+?>
