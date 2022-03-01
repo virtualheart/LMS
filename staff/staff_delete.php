a6c4b1a1f08e22c48a4a49152bc37be4b552e62d
virtualheart <dhanajayan99@gmai.com>
2022-03-01 09:19:17 +0530

    arranged

--- staff/staff_delete.php
+++ staff/staff_delete.php
@@ -1,17 +1,16 @@
 <?php
 
-include"../include/config.php";
+	include"../include/config.php";
 
-session_start();
+	if ($_SESSION["role"]='admin') {
 
-if ($_SESSION["role"]='admin') {
+	$qry="delete from staff where sid={$_GET['id']}";
 
-echo $qry="delete from staff where sid={$_GET['id']}";
-
-if($con->query($qry)){
+	if($con->query($qry)){
+	
 		header("location:view_staff.php?msg=Delete Successfully");		
 
+		}
 	}
-}
 
 ?>
